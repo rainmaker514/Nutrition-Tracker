3ff5009676945e33dc11668cfe691af93928ce80
rainmaker514 <tdoug016@fiu.edu>
2023-02-17 22:00:43 -0500

    added web client

--- Nutrition Tracker/Nutrition-Tracker-User-Service/Nutrition-Tracker-User-Service/src/main/java/com/nutritiontracker/NutritionTrackerUserService/service/EntryServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.nutritiontracker.NutritionTrackerUserService.service;
-
-import com.nutritiontracker.NutritionTrackerUserService.exception.domain.UserNotFoundException;
-import com.nutritiontracker.NutritionTrackerUserService.model.Entry;
-
-public interface EntryServiceInterface {
-    Entry createEntry(Long userId, Entry newEntry) throws UserNotFoundException;
-}
