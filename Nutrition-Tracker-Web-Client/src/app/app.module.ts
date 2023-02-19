46d8cdaa49a8c9ffac9d241014122d701f42d97f
rainmaker514 <tdoug016@fiu.edu>
2023-02-19 10:15:18 -0500

    added about page

--- Nutrition-Tracker-Web-Client/src/app/app.module.ts
+++ Nutrition-Tracker-Web-Client/src/app/app.module.ts
@@ -5,12 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HealthCalculatorComponent } from './pages/health-calculator/health-calculator.component';
+import { InfoComponent } from './pages/info/info.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MealPlannerComponent } from './pages/meal-planner/meal-planner.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    HealthCalculatorComponent,
+    InfoComponent,
+    LoginComponent,
+    MealPlannerComponent,
+    SignupComponent,
+    UserComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
---
+++ Nutrition-Tracker-Web-Client/src/app/pages/health-calculator/health-calculator.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HealthCalculatorComponent } from './health-calculator.component';
+
+describe('HealthCalculatorComponent', () => {
+  let component: HealthCalculatorComponent;
+  let fixture: ComponentFixture<HealthCalculatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HealthCalculatorComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HealthCalculatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
